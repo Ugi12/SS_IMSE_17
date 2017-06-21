b273566ad8337e495ee477507bb4205ba6535594
Julian Thaler <a1228747@unet.univie.ac.at>
2017-06-21 12:26:49 +0200

    update registrierung (funktioniert leider immer noch nicht)

--- ShopSecret/src/controller/RegistrierungController.java
+++ ShopSecret/src/controller/RegistrierungController.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpSession;
 import dao.CustomerDAO;
 import dao.DBManager;
 import model.Customer;
-import model.Product;
 
 @WebServlet("/register")
 public class RegistrierungController extends HttpServlet {
@@ -35,56 +34,29 @@ public class RegistrierungController extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		CustomerDAO customerdao = new CustomerDAO(null);
-		
-		//Daten werden von registrierung.jsp geholt
-		String firstname =request.getParameter("Vorname");
-		String lastname =request.getParameter("Nachname");
-		String email =request.getParameter("RegisterEmail");
-		String birthday =request.getParameter("Geburtstag");
-		String address =request.getParameter("Adresse");
-		String city =request.getParameter("Stadt");
-		String country =request.getParameter("Land");
-		String password =request.getParameter("Passwort");
-		String password2 = request.getParameter("Passwort2");
-		HttpSession session = request.getSession();
-		
-		
-		if(!password.equals(password2)){
-			throw new ServletException("Passw�rter sind nicht gleich!");
+		if(request.getParameter("event").equals("Registrieren")) {
+			Customer c = new Customer();
+			String firstname =request.getParameter("Vorname");
+			String lastname =request.getParameter("Nachname");
+			String email =request.getParameter("RegisterEmail");
+			String birthday =request.getParameter("Geburtstag");
+			String address =request.getParameter("Adresse");
+			String city =request.getParameter("Stadt");
+			String country =request.getParameter("Land");
+			String password =request.getParameter("Passwort");
+			String password2 = request.getParameter("Passwort2");
+			
+			c.setFirstname(firstname);
+			c.setLastname(lastname);
+			c.setEmail(email);
+			c.setBirthday(birthday);
+			c.setAddress(address);
+			c.setCity(city);
+			c.setCountry(country);
+			c.setPassword(password);
+			c.setPassword2(password2);
+			
+			db.getCustomerDAO().create(c);
 		}
-		
-		for (int i = 0; i<customerdao.findAll().size();i++) {
-			if(customerdao.findAll().get(i).getLastname().equals(lastname)) {
-				throw new ServletException("Benutzer mit diesem Namen gibt es schon!");
-			}
-		}
-
-		for (int i = 0; i<customerdao.findAll().size();i++) {
-			if(customerdao.findAll().get(i).getEmail().equals(email)) {
-				throw new ServletException("Benutzer mit dieser E-Mail gibt es schon!");
-			}
-		}
-		
-		//hoechste id wird gesucht, neuer user bekommt eine id um 1 hoeher
-		int id = 0;
-		for (int i = 0; i<customerdao.findAll().size(); i++) {
-			if(customerdao.findAll().get(i).getId() > id) {
-			      id = customerdao.findAll().get(i).getId();
-			}
-		}
-		id++;
-		session.setAttribute("Id", id);
-		
-		customerdao.create();
-		//email, password, password2, firstname, lastname, address, city, country, birthday
-		
-		
-		//folgender befehl wird benoetigt um email im darauffolgenden jsp zu bekommen		
-		session.setAttribute("loginEmail", email);
-		Customer customer = customerdao.findById(id);
-		session.setAttribute("Customer", customer);
-		
-		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 }
